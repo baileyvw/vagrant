0581c9dfa5b9998c6d246dfc8278d261dfd6f38e
ansible bailey <baileyvw@dns-direct.org.uk>
2017-03-22 18:01:52 +0000

    Update or the provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # Install Git and Nano
-yum install -y git
-yum install -y nano
-yum update -y
+#yum install -y git
+#yum install -y nano
+#yum update -y
+echo " Updating system"
+sudo yum update -y 
+echo " Installing git unzip and screen"
+sudo yum install -y git unzip screen nano
+echo " yum update system"
+sudo yum update -y 
+
