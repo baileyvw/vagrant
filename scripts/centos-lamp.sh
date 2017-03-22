bc6bbbe0541f836e85eaf9f3321226b0e9f281b8
ansible bailey <baileyvw@dns-direct.org.uk>
2017-03-22 18:08:04 +0000

    Update the lamp script to remove database work

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -21,13 +21,13 @@ service httpd start
 yum install -y php php-cli php-common php-devel php-mysql
 
 # MySQL
-yum install -y mysql mysql-server mysql-devel
-chkconfig --add mysqld
-chkconfig mysqld on
-
-service mysqld start
-
-mysql -u root -e "SHOW DATABASES";
+#yum install -y mysql mysql-server mysql-devel
+#chkconfig --add mysqld
+#chkconfig mysqld on
+#
+#service mysqld start
+#
+#mysql -u root -e "SHOW DATABASES";
 
 # Download Starter Content
 cd /vagrant
